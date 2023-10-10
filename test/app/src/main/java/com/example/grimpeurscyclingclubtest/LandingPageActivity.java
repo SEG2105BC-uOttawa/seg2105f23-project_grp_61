b8fbb2d95d3ee2eacca2043dbe5408cb3356fde1
Andrei Sburlan <144611823+asburlanUO@users.noreply.github.com>
2023-10-10 11:35:14 -0400

    loging and landing page work

--- test/app/src/main/java/com/example/grimpeurscyclingclubtest/LandingPageActivity.java
+++ test/app/src/main/java/com/example/grimpeurscyclingclubtest/LandingPageActivity.java
@@ -3,6 +3,13 @@ package com.example.grimpeurscyclingclubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LandingPageActivity extends AppCompatActivity {
 
@@ -10,5 +17,40 @@ public class LandingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Bundle bundle = getIntent().getExtras();
+        String uname = bundle.getString("uname");
+
+        TextView textView = findViewById(R.id.landingPageTextVIew);
+
+        textView.setText(uname);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance("https://grimpeurscyclingclubtest-default-rtdb.firebaseio.com/");
+        DatabaseReference roleRef = db.getReference("users/"+uname + "/role");
+
+
+
+        ValueEventListener roleListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                String role = dataSnapshot.getValue(String.class);
+                // ..
+
+                textView.setText("Welcome " + uname + ", you are logged in as " + role);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        roleRef.addValueEventListener(roleListener);
+
+
+
     }
 }
