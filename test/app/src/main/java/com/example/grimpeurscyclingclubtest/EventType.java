edc902c81748ab4e80173d17f9b21e7211637ffc
Integer-Conversion-Error <122304021+Integer-Conversion-Error@users.noreply.github.com>
2023-11-21 18:14:47 -0500

    Made EventType into an object which no longer uses anything other than strings

--- test/app/src/main/java/com/example/grimpeurscyclingclubtest/EventType.java
+++ test/app/src/main/java/com/example/grimpeurscyclingclubtest/EventType.java
@@ -4,43 +4,43 @@ import static java.lang.Integer.parseInt;
 
 public class EventType { // This object will be related to (generalized from?) specifically scheduled events that have info on route and datetime in the future.
 
-    private Integer ageReq;
-    private Double paceReq;
-    private Integer levelReq;
+//    private Integer ageReq;
+//    private Double paceReq;
+//    private Integer levelReq;
     private String title;
     private String description;
 
     public EventType() {}
 
-    public EventType(int ageReq, double paceReq, int levelReq, String title, String description) {
-        this.ageReq = ageReq;
-        this.paceReq = paceReq;
-        this.levelReq = levelReq;
+    public EventType(String title, String description) { //int ageReq, double paceReq, int levelReq,
+//        this.ageReq = ageReq;
+//        this.paceReq = paceReq;
+//        this.levelReq = levelReq;
         this.title = title;
         this.description = description;
     }
 
-    public EventType(String ageReq, String paceReq, String levelReq, String title, String description) {
-        if (ageReq.equals("") || paceReq.equals("") || levelReq.equals("") || title.equals("") || description.equals("")) {
-            return;
-        }
-        try {
-            this.ageReq = parseInt(ageReq);
-            this.paceReq = Double.parseDouble(paceReq);
-            this.levelReq = parseInt(levelReq);
-            this.title = title;
-            this.description = description;
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-    }
+//    public EventType(String ageReq, String paceReq, String levelReq, String title, String description) {
+//        if (ageReq.equals("") || paceReq.equals("") || levelReq.equals("") || title.equals("") || description.equals("")) {
+//            return;
+//        }
+//        try {
+//            this.ageReq = parseInt(ageReq);
+//            this.paceReq = Double.parseDouble(paceReq);
+//            this.levelReq = parseInt(levelReq);
+//            this.title = title;
+//            this.description = description;
+//        } catch (Exception e) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//    }
 
     public boolean isEmpty() {
-        return ageReq == null && paceReq == null && levelReq == null  && title == null && description == null;
+        return title == null && description == null; //ageReq == null && paceReq == null && levelReq == null
     }
 
-    public double getPaceReq() {
+    //public double getPaceReq() {
         return paceReq;
     }
 
@@ -52,25 +52,25 @@ public class EventType { // This object will be related to (generalized from?) s
         return  title;
     }
 
-    public int getAgeReq() {
-        return ageReq;
-    }
+//    public int getAgeReq() {
+//        return ageReq;
+//    }
 
-    public int getLevelReq() {
-        return levelReq;
-    }
+//    public int getLevelReq() {
+//        return levelReq;
+//    }
 
-    public void setAgeReq(int ageReq) {
-        this.ageReq = ageReq;
-    }
+//    public void setAgeReq(int ageReq) {
+//        this.ageReq = ageReq;
+//    }
 
-    public void setLevelReq(int levelReq) {
-        this.levelReq = levelReq;
-    }
+//    public void setLevelReq(int levelReq) {
+//        this.levelReq = levelReq;
+//    }
 
-    public void setPaceReq(double paceReq) {
-        this.paceReq = paceReq;
-    }
+//    public void setPaceReq(double paceReq) {
+//        this.paceReq = paceReq;
+//    }
 
     public void setDescription(String description) {
         this.description = description;
