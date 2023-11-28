9af714ae298b5d21b1fc3133761f2c46670f1ca4
Andrei Sburlan <144611823+asburlanUO@users.noreply.github.com>
2023-11-27 19:03:00 -0500

    implemented all parts

---
+++ test/app/src/main/java/com/example/grimpeurscyclingclubtest/Event.java
@@ -0,0 +1,52 @@
+package com.example.grimpeurscyclingclubtest;
+
+
+import androidx.versionedparcelable.ParcelImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Event extends EventType{
+
+
+    private String eventName;
+
+    private String organizer;
+    private String eventDate;
+    private String eventTime;
+    private int participantLimit;
+    private String registrationFee;
+
+    private String route;
+
+    //private List<ParticipantAccount> participants;
+
+    public Event(String eventType, String organizer, String eventName, String eventDate, String eventTime, int participantLimit, String registrationFee, String route){
+        super(eventType);
+
+
+        this.eventName = eventName;
+        this.organizer = organizer;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.participantLimit = participantLimit;
+        this.registrationFee = registrationFee;
+
+        this.route = route;
+        //this.participants = new ArrayList<ParticipantAccount>();
+    }
+
+    public String getEventName(){return eventName;}
+
+    public String getEventOrganizer(){return organizer;}
+    public String getEventDate(){return eventDate;}
+
+    public String getEventTime(){return eventTime;}
+
+    public int getParticipantLimit(){return participantLimit;}
+
+    public String getRegistrationFee(){return registrationFee;}
+
+    public String  getRouteInfo(){return route;}
+}
--- test/app/src/main/java/com/example/grimpeurscyclingclubtest/TextInputValidation.java
+++ test/app/src/main/java/com/example/grimpeurscyclingclubtest/TextInputValidation.java
@@ -130,4 +130,47 @@ public class TextInputValidation {
         return matcher.find();
     }
 
+
+    public static boolean validateRegistrationFee(String input){
+        if (input == null){
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("^\\d{1,3}(\\.\\d{1,2})?$");// 0-999.99
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
+    }
+
+    public static boolean validateParticipantLimit(String input){
+        if (input == null){
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("^([1-9][0-9]{0,2}|1000)$");//1-1000
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
+    }
+    public static boolean validate24hrTime(String input){
+        if (input == null){
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile("^(?:[01]?\\d|2[0-3])(?:[0-5]\\d){1,2}$");//0000-2359
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
+    }
+    public static boolean validateDate(String input){
+        if (input == null){
+            return false;
+        }
+        //matches DD/M/YYYY
+        Pattern pattern = Pattern.compile("(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})");//0000-2359
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
+    }
+
 }
