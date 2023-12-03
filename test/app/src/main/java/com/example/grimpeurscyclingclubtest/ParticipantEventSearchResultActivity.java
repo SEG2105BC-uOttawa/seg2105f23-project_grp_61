ff0b752fce88da4d7e2e85184a818cf36a8644c1
Andrei Sburlan <144611823+asburlanUO@users.noreply.github.com>
2023-12-02 19:41:53 -0500

    finished search screen, todo join event screen

---
+++ test/app/src/main/java/com/example/grimpeurscyclingclubtest/ParticipantEventSearchResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.grimpeurscyclingclubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParticipantEventSearchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participant_event_search_result);
+    }
+}
