5f7fe6fcf9b8baa9faf4251cd3c5411dc6bbff99
Integer-Conversion-Error <122304021+Integer-Conversion-Error@users.noreply.github.com>
2023-12-04 14:49:34 -0500

    some organizer things

--- test/app/src/main/java/com/example/grimpeurscyclingclubtest/OrganizerAccount.java
+++ test/app/src/main/java/com/example/grimpeurscyclingclubtest/OrganizerAccount.java
@@ -1,7 +1,12 @@
 package com.example.grimpeurscyclingclubtest;
 
 public class OrganizerAccount extends Account{
+    public String logoImage;
+
     public OrganizerAccount(String username) {
         super(username);
+    }
+    public void addLogoImage(String newLogo){
+        logoImage = newLogo;
     }
 }
